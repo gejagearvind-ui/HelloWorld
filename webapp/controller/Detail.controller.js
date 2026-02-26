6ecdeb727c4e80403347410115c6db448f420596
Aravind Gejage <gejage.arvind@gmail.com>
2026-02-26 05:43:41 +0000

    Initial UI5 project structure

---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,37 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History",
+    "sap/ui/model/json/JSONModel"
+], (Controller, History, JSONModel) => {
+	"use strict";
+
+	return Controller.extend("ui5.walkthrough.controller.Detail", {
+		onInit() {
+            const oViewModel = new JSONModel({
+				currency: "EUR"
+			});
+			this.getView().setModel(oViewModel, "view");
+
+			const oRouter = this.getOwnerComponent().getRouter();
+			oRouter.getRoute("detail").attachPatternMatched(this.onObjectMatched, this);
+		},
+
+		onObjectMatched(oEvent) {
+			this.getView().bindElement({
+				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
+				model: "invoice"
+			});
+		},
+        onNavBack() {
+			const oHistory = History.getInstance();
+			const sPreviousHash = oHistory.getPreviousHash();
+
+			if (sPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				const oRouter = this.getOwnerComponent().getRouter();
+				oRouter.navTo("overview", {}, true);
+			}
+		}
+	});
+});
